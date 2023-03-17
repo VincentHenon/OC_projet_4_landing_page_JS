70c4599ce1b7d97aee99d08874a4d76fa1ff8dfb
Vincent Henon <mr.henonvincent@gmail.com>
2023-03-17 10:03:50 +0100

    issue#1 close modal=done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,22 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//close modal event
+modalClose.addEventListener("click", closeModalClick)
+window.addEventListener("keydown", closeModalKey)
+
+//close modal function on click
+function closeModalClick() {
+  modalbg.style.display = "none";
+}
+
+//close modal function on keydown
+function closeModalKey(e) {
+  if(e.key === "Escape") {
+    modalbg.style.display = "none";
+  }
 }
 
 

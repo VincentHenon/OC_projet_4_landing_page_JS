f9cf33d36842b77a7cac03d6950c5dce6f39503e
Vincent Henon <mr.henonvincent@gmail.com>
2023-03-17 14:41:03 +0100

    validation en cours

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,20 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+const firstName = document.getElementById("first");
+const lastName =  document.getElementById("last");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+//const location = document.getElementById("location");
+const submitBtn = document.querySelector(".btn-submit");
+// Regex pattern
+const nameReg = "";
+const birthReg = "";
+const quantityReg = "";
+
+
+////_________________////
+//// MODAL HANDLING ////
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,20 +35,30 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close modal event
+// close modal event
 modalClose.addEventListener("click", closeModalClick)
 window.addEventListener("keydown", closeModalKey)
 
-//close modal function on click
+// close modal function on click
 function closeModalClick() {
   modalbg.style.display = "none";
 }
 
-//close modal function on keydown
+// close modal function on keydown
 function closeModalKey(e) {
   if(e.key === "Escape") {
     modalbg.style.display = "none";
   }
 }
 
+////__________________////
+//// FORM VALIDATION ////
+
+// submit button on click
+submitBtn.addEventListener("click", submitForm)
+
+// submit function
+function submitForm() {
+
+}
 
